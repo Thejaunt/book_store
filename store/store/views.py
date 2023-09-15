2dbac3ae421e410420bc26ad4962e2e2b5c49b5d
Thejaunt <thejauntthejaunt@gmail.com>
2023-09-15 03:25:48 +0300

    updates

--- store/store/views.py
+++ store/store/views.py
@@ -26,23 +26,27 @@ class BookDetailView(generics.RetrieveAPIView, generics.CreateAPIView):
     serializer_class = BookDetailSerializer
 
     def create(self, request, *args, **kwargs):
-        """Adding book into Cart"""
+        """Adding a book into the Cart"""
         obj = self.get_object()
         order_instance, created = Order.objects.get_or_create(customer=request.user, status="CART")
 
         order_items = [oi for oi in order_instance.orderitem_set.all()]
+        # if the book is already in the cart - increment the cart quantity
         for order_item in order_items:
             if order_item.book == obj:
-                order_item.quantity += 1
-                order_item.save()
+                if order_item.quantity < obj.quantity:
+                    order_item.quantity += 1
+                    order_item.save()
                 return Response({"message": "book has been added to the cart"}, status=status.HTTP_200_OK)
-
-        OrderItem.objects.create(
-            book=obj,
-            order=order_instance,
-            customer=request.user,
-            quantity=1,
-        )
+        if obj.quantity > 0:
+            OrderItem.objects.create(
+                book=obj,
+                order=order_instance,
+                customer=request.user,
+                quantity=1,
+            )
+        else:
+            return Response(data={f"{obj.title} is out of stock. Can't add it into the CART"})
         return Response({"message": "book has been added to the cart"}, status=status.HTTP_201_CREATED)
 
 
